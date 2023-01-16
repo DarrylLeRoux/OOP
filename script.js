4f3db29f2da62da7a2b490c6b0324472aeeefd58
Darryl Le Roux <devdarryl@outlook.com>
2023-01-16 15:18:23 +0200

    Added the prototype chain

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 'use strict';
 
 // Constructor function
-
+////////////////////////
 const Person = function (firstName, birthYear) {
   // Instance properties
   this.firstName = firstName;
   this.birthYear = birthYear;
+
+  // Never create a method inside a constructor function
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
 };
 
 const Jonas = new Person('Jonas', 1991);
@@ -23,3 +28,30 @@ console.log(Matilda);
 console.log(Jack);
 
 console.log(Jonas instanceof Person); // true
+
+// Prototypes
+////////////////////////
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2022 - this.birthYear);
+};
+
+// calcAge() is now available on the Person prototype, can can be called when needed, and is not automatically applied to each instance
+
+Jonas.calcAge();
+// if we try and call a method on an Object that doesn't exist in the Object, it will then look at the prototype to see if it is there
+// Jonas =
+// name: Jonas
+// birthYear: 1990
+// __proto__: Person.prototype
+// calcAge() exists on the Person prototype, and therefore has access to it
+
+// __proto__ points to the Person's prototype
+console.log(Jonas.__proto__ === Person.prototype);
+console.log(Jonas.__proto__); // will have all the methods on the Person prototype
+console.log(Jonas.__proto__.__proto__); // prototype chain can then go as far as Object
+
+const arr = [1, 3, 45, 5, 5, 6, 6, 6, 6, 6];
+console.log(arr.__proto__); //map, find, filter, reduce etc.
+console.log(arr.__proto__ === Array.prototype); //true
