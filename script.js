85f0c2d478e468f3900eda6c4434a304650e3194
Darryl Le Roux <devdarryl@outlook.com>
2023-01-16 15:44:42 +0200

    Added the coding challenge of implementing a constructur function

--- script.js
+++ script.js
@@ -55,3 +55,43 @@ console.log(Jonas.__proto__.__proto__); // prototype chain can then go as far as
 const arr = [1, 3, 45, 5, 5, 6, 6, 6, 6, 6];
 console.log(arr.__proto__); //map, find, filter, reduce etc.
 console.log(arr.__proto__ === Array.prototype); //true
+
+///////////////////
+// Coding challenge
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+Car.prototype.accelerate = function () {
+  console.log(
+    `The ${this.make} has accelerated to ${
+      this.speed + 10
+    } km/h`
+  );
+};
+Car.prototype.brake = function () {
+  console.log(
+    `The ${this.make} has decreased its speed to ${
+      this.speed - 5
+    } km/h`
+  );
+};
+
+bmw.accelerate();
+bmw.brake();
